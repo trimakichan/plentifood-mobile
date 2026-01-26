3cb5f465d0b030ba1ebff9634fc2fad1b15227bb
Makiko <trimakichan@gmail.com>
2026-01-25 21:38:41 -0800

    complete Home ui

--- app/src/main/java/com/example/plentifood/ui/screens/Home.kt
+++ app/src/main/java/com/example/plentifood/ui/screens/Home.kt
@@ -2,7 +2,6 @@ package com.example.plentifood.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,14 +20,12 @@ import androidx.compose.ui.unit.dp
 import com.example.plentifood.R
 import com.example.plentifood.ui.composables.PrimaryButton
 import com.example.plentifood.ui.composables.SecondaryButton
-import com.example.plentifood.ui.composables.SingleChoiceSegmentedButton
 import com.example.plentifood.ui.theme.PlentifoodTheme
-
 
 
 @Composable
 fun Home (modifier: Modifier = Modifier) {
-    Column(modifier = Modifier
+    Column(modifier = modifier
         .fillMaxSize()
         .padding(24.dp),
         verticalArrangement = Arrangement.SpaceAround
@@ -35,7 +33,7 @@ fun Home (modifier: Modifier = Modifier) {
 
         Text("Logo")
 
-        Spacer(modifier = Modifier.padding(24.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
 
         Text("Welcome to the PlentiFood",
@@ -50,21 +48,21 @@ fun Home (modifier: Modifier = Modifier) {
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(350.dp)
+                .height(350.dp),
+            contentScale = ContentScale.Crop
         )
 
-        Column (modifier = Modifier
-        ) {
+        Column {
             PrimaryButton("Lets begin", onClick = {})
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.height(14.dp))
             SecondaryButton("Staff Login", onClick = {} )
         }
 
-        Text("Don't have a staff account as a staff? Sign up")
-
-
-
-
+        Text("Don't have a staff account as a staff? Sign up",
+                modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.labelLarge,
+                textAlign = TextAlign.Center
+            )
 
     }
 }
--- app/src/main/java/com/example/plentifood/ui/theme/Type.kt
+++ app/src/main/java/com/example/plentifood/ui/theme/Type.kt
@@ -2,14 +2,34 @@ package com.example.plentifood.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.plentifood.R
+
+val lora = FontFamily(
+    Font(R.font.lora, FontWeight.Normal),
+    Font(R.font.lora_italic, FontWeight.Normal)
+)
+
+val roboto = FontFamily(
+    Font(R.font.roboto, FontWeight.Normal),
+    Font(R.font.roboto_italic, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -18,12 +38,21 @@ val Typography = Typography(
 //    Other default text styles to override
 
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = lora,
+        fontWeight = FontWeight.Bold,
         fontSize = 36.sp,
         lineHeight = 38.sp,
         letterSpacing = 0.sp
     ),
+
+    titleMedium = TextStyle(
+        fontFamily = lora,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
